b8e4df5856268f24ec79af29a0fa575e9776bbd9
yzm1205 <yash.mahajan50@gmail.com>
2024-09-25 00:55:45 -0500

    version updated v.0.2.12

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="shard_llms",
-    version="0.2.11",
+    version="0.2.12",
     author="Yzm0034",
     author_email="yash.mahajan50@gmail.com",
     description="Shard Large Language Models",
